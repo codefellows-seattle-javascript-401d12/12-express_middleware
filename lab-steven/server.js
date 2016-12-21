a28faea8d0e2b86387ecd5f16daf169b3faf2843
Steven Bateman <batemanvo@gmail.com>
2016-12-20 19:17:57 -0800

    Required in new middleware in server.js.

--- lab-steven/server.js
+++ lab-steven/server.js
@@ -2,10 +2,14 @@
 
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('./lib/cors-middleware.js');
+const error = require('./error-middleware.js');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(morgan('dev'));
+app.use(cors);
+app.use(error);
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
