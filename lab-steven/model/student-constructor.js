88994accfa8a930b9ae12967dd1f5b268b09e865
Steven Bateman <batemanvo@gmail.com>
2016-12-20 18:07:15 -0800

    Updated constructor with debug and error handling.

--- lab-steven/model/student-constructor.js
+++ lab-steven/model/student-constructor.js
@@ -1,8 +1,15 @@
 'use strict';
 
 const uuid = require('node-uuid');
+const debug = require('debug')('student:constructor');
+const createError = require('http-errors');
 
 const Student = module.exports = function(student) {
+  debug('Student constructor.');
+
+  if (!student.name) throw createError(400, 'Expected a name field.');
+  if (!student.age) throw createError(400, 'Expected an age field.');
+  
   this.id = uuid.v1();
   for (var key in student) {
     this[key] = student[key];
