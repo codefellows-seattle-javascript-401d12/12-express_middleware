bad4f17efa30e64785b07006d35e84178d800708
kcirekcom <erick.f.mock@gmail.com>
2016-12-20 20:29:18 -0800

    created post and get routers in pin-router

---
+++ route/pin-router.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const Router = require('express').Router;
+const jsonParser = require('body-parser').json();
+const debug = require('debug')('pin:pin-router');
+const Pin = require('../model/pin.js');
+const pinRouter = new Router();
+
+pinRouter.post('api/pin', jsonParser, function(req, res, next) {
+  debug('POST: /api/pin');
+
+  Pin.createPin(req.body)
+  .then(pin => res.json(pin))
+  .catch(err => next(err));
+});
+
+pinRouter.get('/api/pin/:id', function(req, res, next) {
+  debug('GET: /api/pin/:id');
+
+  Pin.fetchPin(req.params.id)
+  .then(pin => res.json(pin))
+  .catch(err => next(err));
+});
+
+pinRouter.get('/api/pin', function(req, res, next) {
+  debug('GET: /api/pin');
+
+  Pin.fetchIDs()
+  .then(ids => res.json(ids))
+  .catch(next);
+});
