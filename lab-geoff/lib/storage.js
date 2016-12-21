a4fd72efba73dcac162b73cf187bc399d6729e79
Geoff Simons <geoff.simons@gmail.com>
2016-12-20 18:42:02 -0800

    Added debug to storage

--- lab-geoff/lib/storage.js
+++ lab-geoff/lib/storage.js
@@ -3,12 +3,13 @@
 const uuid = require('node-uuid');
 const Promise = require('bluebird');
 const createError = require('http-errors');
-//TODO: Use debug
+const debug = require('debug')('mnp:storage');
 const fs = Promise.promisifyAll(require('fs'), {suffix: 'Prom'});
 
 module.exports = exports = {};
 
 exports.createItem = function(collection, item) {
+  debug('createItem()', item);
   if(!collection) return Promise.reject(createError(400, 'collection name not supplied'));
   if(!item) return Promise.reject(createError(400, 'missing item to create'));
 
@@ -21,6 +22,7 @@ exports.createItem = function(collection, item) {
 };
 
 exports.fetchItem = function(collection, id) {
+  debug('fetchItem()', collection, id);
   if(!collection) return Promise.reject(createError(400, 'collection name not supplied'));
   if(!id) return Promise.reject(createError(400, 'missing id'));
 
@@ -33,15 +35,16 @@ exports.fetchItem = function(collection, id) {
 };
 
 exports.deleteItem = function(collection, id) {
+  debug('deleteItem()', collection, id);
   if(!collection) return Promise.reject(createError(400, 'collection name not supplied'));
   if(!id) return Promise.reject(createError(400, 'missing id'));
 
-  //TODO: Wrap the promise then and catch for better messaging?
   return fs.unlinkProm(`./data/${collection}/${id}.json`)
   .catch( err => Promise.reject(createError(404, err.message)));
 };
 
 exports.updateItem = function(collection, id, _item) {
+  debug('updateItem()',collection,id,_item);
   if(!collection) return Promise.reject(createError(400, 'collection name not supplied'));
   if(!id) return Promise.reject(createError(400, 'missing id'));
 
@@ -51,13 +54,21 @@ exports.updateItem = function(collection, id, _item) {
       if(prop === 'id') continue;
       if(_item[prop]) item[prop] = _item[prop];
     }
+    // Calling createItem seems weird here, but we are essentially
+    // just overwriting the object that was there.
     return this.cteateItem(collection, item);
   })
   .catch( err => Promise.reject(createError(404, err.message)));
 };
 
 exports.listItems = function(collection) {
+  debug('listItems()');
   return fs.readdirProm(`./data/${collection}`)
-  .then( files => files.map(name => name.split('.json')[0]))
+  .then( files => files
+  .filter( name => {
+    debug('filter:',name);
+    return name !== '.keep';
+  })
+  .map(name => name.split('.json')[0]))
   .catch( err => Promise.reject(createError(404, err.message)));
 };
