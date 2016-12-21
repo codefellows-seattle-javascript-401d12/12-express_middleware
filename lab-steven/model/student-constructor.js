87cf710c1da6bc6633a3c4faebc63f4dd7218479
Steven Bateman <batemanvo@gmail.com>
2016-12-20 19:10:02 -0800

    Added getStudent method to Student constructor.

--- lab-steven/model/student-constructor.js
+++ lab-steven/model/student-constructor.js
@@ -3,15 +3,21 @@
 const uuid = require('node-uuid');
 const debug = require('debug')('student:constructor');
 const createError = require('http-errors');
+const storage = require('../lib/storage.js');
 
 const Student = module.exports = function(student) {
   debug('Student constructor.');
 
   if (!student.name) throw createError(400, 'Expected a name field.');
   if (!student.age) throw createError(400, 'Expected an age field.');
-  
+
   this.id = uuid.v1();
   for (var key in student) {
     this[key] = student[key];
   }
+};
+
+Student.createStudent = function(student) {
+  debug('Student constructor: createStudent method.');
+  return storage.createItem('student', new Student(student));
 };
