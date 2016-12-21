bea83efaf109ca30606a7fb42dfc56409bb82005
Steven Bateman <batemanvo@gmail.com>
2016-12-20 18:35:41 -0800

    Added getItem method to storage.js.

--- lab-steven/lib/storage.js
+++ lab-steven/lib/storage.js
@@ -15,6 +15,17 @@ exports.createItem = function(schemaName, item) {
 
   let stringifiedItem = JSON.stringify(item);
   return fs.writeFileProm(`${__dirname}/../data/${schemaName}/${item.id}.json`, stringifiedItem)
-  .then(() => student)
-  .catch(err => Promise.reject(createError(400, err.message)));
+  .then(() => item)
+  .catch(err => Promise.reject(createError(500, err.message)));
+};
+
+exports.readItem = function(schemaName, id) {
+  debug('Storage.js: readItem method.');
+
+  if (!schemaName) throw createError(400, 'Expected a schema name.');
+  if (!id) throw createError(400, 'Expected an ID.');
+
+  return fs.readFileProm(`${__dirname}/../data/${schemaName}/${id}.json`)
+  .then(data => JSON.parse(data.toString()))
+  .catch(err => Promise.reject(createError(404, err.message)));
 };
