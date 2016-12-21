ec17a5317fd246ce435d7a42117057a00a8aa772
Steven Bateman <batemanvo@gmail.com>
2016-12-20 18:00:39 -0800

    Created initial server file.

--- lab-steven/server.js
+++ lab-steven/server.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const express = require('express');
+const morgan = require('morgan');
+const debug = require('debug');
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(morgan('dev'));
+
+app.listen(PORT, () => {
+  debug(`Server running on port ${PORT}`);
+});
