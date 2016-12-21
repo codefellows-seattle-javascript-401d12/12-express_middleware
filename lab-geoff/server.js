8f280841d595f12d1dc2a6795723bc8404f3ae64
Geoff Simons <geoff.simons@gmail.com>
2016-12-20 17:41:18 -0800

    Server now uses middleware

--- lab-geoff/server.js
+++ lab-geoff/server.js
@@ -8,6 +8,11 @@ const PORT = process.env.PORT || 8000;
 const app = express();
 
 app.use(morgan('dev'));
+app.use(require('./lib/cors-middleware.js'));
+
+//TODO: Add router
+
+app.use(require('./lib/error-middleware.js'));
 
 app.list(PORT, () => {
   debug('Server up:', PORT);
