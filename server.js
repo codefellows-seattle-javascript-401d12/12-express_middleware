500488c506821fbec5f565866253372627ae7d9a
Daniel Becker <dbecker4130@gmail.com>
2016-12-20 17:39:55 -0800

    all server dependencies required in

--- server.js
+++ server.js
@@ -2,13 +2,19 @@
 
 const morgan = require('morgan');
 const express = require('express');
-const debug = require('debug')('note:server');
+const createError = require('http-errors');
+const cors = require('./lib/cors-middleware.js');
+const errors = require('./lib/err-middleware.js');
+const debug = require('debug')('artist:server');
 const artistRouter = require('./route/artist-router.js');
 
 const PORT = 3000;
 const app = express();
 
 app.use(morgan('dev'));
+app.use(cors);
+app.use(artistRouter);
+app.use(errors);
 
 app.listen(PORT, () => {
   console.log(`server live on: ${PORT}`);
