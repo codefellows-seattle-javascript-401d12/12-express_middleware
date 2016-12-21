ce31a45379c4c84ff90c2ea695e2b70e394280a9
kcirekcom <erick.f.mock@gmail.com>
2016-12-20 20:07:42 -0800

    cors-middleware built, required in server

--- server.js
+++ server.js
@@ -3,12 +3,14 @@
 const morgan = require('morgan');
 const express = require('express');
 const createError = require('http-errors');
+const cors = require('./lib/cors-middleware.js');
 const debug = require('debug')('pin:server');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(morgan('dev'));
+app.use(cors);
 
 app.listen(PORT, () => {
   console.log(`server running: ${PORT}`);
