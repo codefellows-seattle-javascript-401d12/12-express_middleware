06f5870456e1c1f5e4d6cf7c2c64786c932ead4b
Steven Bateman <batemanvo@gmail.com>
2016-12-20 18:32:17 -0800

    Added createItem method to storage.js.

--- lab-steven/lib/storage.js
+++ lab-steven/lib/storage.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const Promise = require('bluebird');
+const fs = Promise.promisifyAll(require('fs'), {suffix: 'Prom'});
+const createError = require('http-errors');
+const debug = require('debug')('student:storage');
+
+module.exports = exports = {};
+
+exports.createItem = function(schemaName, item) {
+  debug('Storage.js: createItem method.');
+
+  if (!schemaName) throw createError(400, 'Expected a schema name.');
+  if (!item) throw createError(400, 'Expected an item.');
+
+  let stringifiedItem = JSON.stringify(item);
+  return fs.writeFileProm(`${__dirname}/../data/${schemaName}/${item.id}.json`, stringifiedItem)
+  .then(() => student)
+  .catch(err => Promise.reject(createError(400, err.message)));
+};
