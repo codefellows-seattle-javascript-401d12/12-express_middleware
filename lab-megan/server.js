8b640db58e22e6037f4bc58d58a94b0bc7306c74
Megan Reardon <meganr@gmail.com>
2016-12-20 19:59:43 -0800

    added cors and error middleware

--- lab-megan/server.js
+++ lab-megan/server.js
@@ -3,12 +3,16 @@
 const morgan = require('morgan');
 const express = require('express');
 const createError = require('http-errors');
+const cors = require('./lib/cors-middleware.js');
+const errors = require('./lib/error-middleware.js');
 const debug = require('debug')('hat:server');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(morgan('dev'));
+app.use(cors);
+app.use(errors);
 
 app.listen(PORT, () => {
   console.log(`server up at: ${PORT}`);
