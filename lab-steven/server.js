f60c027a15fea4fddae663cfb12f9124b7b37f8e
Steven Bateman <batemanvo@gmail.com>
2016-12-20 18:02:00 -0800

    Replaced debug with console.log in app.listen.

--- lab-steven/server.js
+++ lab-steven/server.js
@@ -2,12 +2,11 @@
 
 const express = require('express');
 const morgan = require('morgan');
-const debug = require('debug');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(morgan('dev'));
 
 app.listen(PORT, () => {
-  debug(`Server running on port ${PORT}`);
+  console.log(`Server started on port ${PORT}`);
 });
