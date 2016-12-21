025fc1325352b830c5a5b2bac9ccb575d3f62f49
kcirekcom <erick.f.mock@gmail.com>
2016-12-20 20:14:18 -0800

    error-middleware built, required in server

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const express = require('express');
 const createError = require('http-errors');
 const cors = require('./lib/cors-middleware.js');
+const errors = require('./lib/error-middleware.js');
 const debug = require('debug')('pin:server');
 
 const PORT = process.env.PORT || 3000;
@@ -11,6 +12,8 @@ const app = express();
 
 app.use(morgan('dev'));
 app.use(cors);
+
+app.use(errors);
 
 app.listen(PORT, () => {
   console.log(`server running: ${PORT}`);
