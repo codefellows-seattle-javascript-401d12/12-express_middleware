94ac0b60c06abab868173282dcd244cbc1c04df8
kcirekcom <erick.f.mock@gmail.com>
2016-12-20 17:30:21 -0800

    added static methods onto pin

--- model/pin.js
+++ model/pin.js
@@ -15,3 +15,43 @@ const Pin = module.exports = function(title, skill) {
   this.title = title;
   this.skill = skill;
 };
+
+Pin.createPin = function(_pin) {
+  debug('createPin');
+
+  try {
+    let pin = new Pin(_pin.title, _pin.skill);
+    return storage.createItem('pin', pin);
+  } catch (err) {
+    return Promise.reject(createError(400, err.message));
+  }
+};
+
+Pin.fetchPin = function(id) {
+  debug('fetchPin');
+  return storage.fetchItem('pin', id);
+};
+
+Pin.updatePin = function(id, _pin) {
+  debug('updatePin');
+
+  return storage.fetchItem('pin', id)
+  .catch(err => Promise.reject(createError(404, err.message)))
+  .then(pin => {
+    for (var prop in pin) {
+      if (prop === 'id') continue;
+      if (_pin[prop]) pin[prop] = _pin[prop];
+    }
+    return storage.createItem('pin', pin);
+  });
+};
+
+Pin.deletePin = function(id) {
+  debug('deletePin');
+  return storage.deleteItem('pin', id);
+};
+
+Pin.fetchIDs = function() {
+  debug('fetchIDs');
+  return storage.availIDs('pin');
+};
