681cb84814db4c0e9d12da23578e8a8c4db45488
Hawa Abdi <abdih17@hotmail.com>
2016-12-22 10:53:14 -0800

    Most test passing.

--- lab-hawa/test/spiritAnimal-route-test.js
+++ lab-hawa/test/spiritAnimal-route-test.js
@@ -19,28 +19,30 @@ describe('Spirit Animal Route', function() {
     describe('with a valid body', function() {
       before( done => {
         SpiritAnimal.createSpiritAnimal(exampleSpiritAnimal)
-        .then.tempSpiritAnimal = spiritAnimal;
-        done();
-      })
-      .catch( err => done(err));
-    });
+        .then(spiritAnimal => {
+          this.tempSpiritAnimal = spiritAnimal;
+          done();
+        })
+        .catch( err => done(err));
+      });
 
-    after( done => {
-      SpiritAnimal.deleteSpiritAnimal(this.tempSpiritAnimal.id)
-      .then( () => done())
-      .catch(done);
-    });
+      after( done => {
+        SpiritAnimal.deleteSpiritAnimal(this.tempSpiritAnimal.id)
+        .then( () => done())
+        .catch(done);
+      });
 
-    it('should return a spirit animal', done => {
-      request.get(`${url}/api/spiritAnimal${this.tempSpiritAnimal.id}`)
-      .end( (err, res) => {
-        if (err) return done(err);
-        expect(res.status).to.equal(200);
-        expect(res.body.id).to.equal(this.tempSpiritAnimal.id);
-        expect(res.body.name).to.equal(this.tempSpiritAnimal.name);
-        expect(res.body.spiritAnimal).to.equal(this.tempSpiritAnimal.spiritAnimal);
-        expect(res.body.spiritAnimalName).to.equal(this.tempSpiritAnimal.spiritAnimalName);
-        done();
+      it('should return a spirit animal', done => {
+        request.get(`${url}/api/spiritAnimal/${this.tempSpiritAnimal.id}`)
+        .end( (err, res) => {
+          if (err) return done(err);
+          expect(res.status).to.equal(200);
+          expect(res.body.id).to.equal(this.tempSpiritAnimal.id);
+          expect(res.body.name).to.equal(this.tempSpiritAnimal.name);
+          expect(res.body.spiritAnimal).to.equal(this.tempSpiritAnimal.spiritAnimal);
+          expect(res.body.spiritAnimalName).to.equal(this.tempSpiritAnimal.spiritAnimalName);
+          done();
+        });
       });
     });
 
@@ -55,62 +57,61 @@ describe('Spirit Animal Route', function() {
     });
   });
 
-  describe('POST: /api/spiritAnimal', function() {
-    describe('with an valid body', function() {
-      after( done => {
-        if(this.tempSpiritAnimal) {
-          SpiritAnimal.deleteItem(this.tempSpiritAnimal.id)
-          .then( () => done())
-          .catch(done);
-        }
-      });
+});
+describe('POST: /api/spiritAnimal', function() {
+  describe('with an valid body', function() {
+    after( done => {
+      if(this.tempSpiritAnimal) {
+        SpiritAnimal.deleteSpiritAnimal(this.tempSpiritAnimal.id)
+        .then( () => done())
+        .catch(done);
+      }
+    });
 
-      it('should return a spiritAnimal', done => {
-        request.post(`${url}/api/spiritAnimal`)
-        .send(exampleSpiritAnimal)
-        .end((err, res) => {
-          if (err) return done(err);
-          expect(res.status).to.equal(200);
-          expect(res.body.name).to.equal(this.exampleSpiritAnimal.name);
-          expect(res.body.spiritAnimal).to.equal(this.exampleSpiritAnimal.spiritAnimal);
-          expect(res.body.spiritAnimalName).to.equal(this.exampleSpiritAnimal.spiritAnimalName);
-          this.tempSpiritAnimal = res.body;
-          done();
-        });
+    it('should return a spiritAnimal', done => {
+      request.post(`${url}/api/spiritAnimal`)
+      .send(exampleSpiritAnimal)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.status).to.equal(200);
+        expect(res.body.name).to.equal(exampleSpiritAnimal.name);
+        expect(res.body.spiritAnimal).to.equal(exampleSpiritAnimal.spiritAnimal);
+        expect(res.body.spiritAnimalName).to.equal(exampleSpiritAnimal.spiritAnimalName);
+        this.tempSpiritAnimal = res.body;
+        done();
       });
     });
   });
+});
 
-  describe('PUT: /api/spiritAnimal', function() {
-    describe('with a valid body and id', function() {
-      before( done => {
-        SpiritAnimal.createSpiritAnimal(exampleSpiritAnimal)
-        .then( spiritAnimal => {
-          this.tempSpiritAnimal = spiritAnimal;
-          done();
-        })
+describe('PUT: /api/spiritAnimal', function() {
+  describe('with a valid body and id', function() {
+    before( done => {
+      SpiritAnimal.createSpiritAnimal(exampleSpiritAnimal)
+      .then( spiritAnimal => {
+        this.tempSpiritAnimal = spiritAnimal;
+        done();
+      })
+      .catch(done);
+    });
+
+    after( done => {
+      if (this.tempSpiritAnimal) {
+        SpiritAnimal.deleteSpiritAnimal(this.tempSpiritAnimal.id)
+        .then( () => done())
         .catch(done);
-      });
+      }
+    });
 
-      after( done => {
-        if (this.tempSpiritAnimal) {
-          SpiritAnimal.deleteSpiritAnimal(this.tempSpiritAnimal.id)
-          .then( () => done())
-          .catch(done);
-        }
-      });
-
-      it('should return a spiritAnimal', done => {
-        let updateSpiritAnimal = { spiritAnimal: 'new name', spiritAnimal: 'new spiritAnimal', spiritAnimalName: 'new spiritAnimalName' };
-        request.put(`${url}/api/spiritAnimal?id=${this.tempSpiritAnimal.id}`)
-        .send(updateSpiritAnimal)
-        .end((err, res) => {
-          if (err) return done(err);
-          expect(res.status).to.equal(200);
-          expect(res.body.id).to.equal(this.tempSpiritAnimal.id);
-          done();
-        }
-      );
+    it('should return a spiritAnimal', done => {
+      let updateSpiritAnimal = { name: 'new name', spiritAnimal: 'new spiritAnimal', spiritAnimalName: 'new spiritAnimalName' };
+      request.put(`${url}/api/spiritAnimal?id=${this.tempSpiritAnimal.id}`)
+      .send(updateSpiritAnimal)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.status).to.equal(200);
+        expect(res.body.id).to.equal(this.tempSpiritAnimal.id);
+        done();
       });
     });
   });
