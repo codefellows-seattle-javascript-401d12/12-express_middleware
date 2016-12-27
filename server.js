9b3d284bb9c44ecc0c1f904f7e2bfe9915cc1313
Caleb Sattgast <calebsattgast@gmail.com>
2016-12-26 23:09:37 -0800

    require in basic dependencies

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 'use strict';
 
+const morgan = require('morgan');
 const express = require('express');
+const createError = require('http-errors');
+const debug = require('debug')('bev:server');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
