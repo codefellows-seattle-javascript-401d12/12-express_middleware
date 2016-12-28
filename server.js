0d85d9a8f675cba84adc49d97f03d7793280a187
Caleb Sattgast <calebsattgast@gmail.com>
2016-12-28 14:05:17 -0800

    create cors-middleware module and include it in server file

--- server.js
+++ server.js
@@ -3,12 +3,14 @@
 const morgan = require('morgan');
 const express = require('express');
 const createError = require('http-errors');
+const cors = require('./lib/cors-middleware.js');
 const debug = require('debug')('bev:server');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(morgan('dev'));
+app.use(cors);
 
 app.listen(PORT, () => {
   console.log(`SERVER RUNNING ON PORT ${PORT}`);
