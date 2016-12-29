deb770c881aa6dd7cc52fa77d705e94b25285e20
Caleb Sattgast <calebsattgast@gmail.com>
2016-12-28 23:39:35 -0800

    add error middleware and BEV router to server file

--- server.js
+++ server.js
@@ -4,13 +4,17 @@ const morgan = require('morgan');
 const express = require('express');
 const createError = require('http-errors');
 const cors = require('./lib/cors-middleware.js');
+const errors = require('./lib/error-middleware.js');
 const debug = require('debug')('bev:server');
+const bevRouter = require('./route/bev-router.js');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(morgan('dev'));
 app.use(cors);
+app.use(bevRouter);
+app.use(errors);
 
 app.listen(PORT, () => {
   console.log(`SERVER RUNNING ON PORT ${PORT}`);
